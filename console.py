21c0531ab4043ec4c80f3fe977f8a7a4d1a284b1
husamahmud <husamahmud@gmail.com>
2023-12-08 19:55:12 +0300

    [feat] Add `create` cmd to `HBNBCommand` class

--- console.py
+++ console.py
@@ -2,11 +2,31 @@
 """Module for the HBNBCommand class and command interpreter"""
 
 import cmd
+import importlib
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB"""
     prompt = '(hbnb) '
+    __classes = ["BaseModel"]
+
+    def do_create(self, line):
+        """
+        Creates a new instance of BaseModel, saves it and prints the id
+        Usage: create <Class Name>
+        """
+        if not line:
+            print("** class name missing **")
+            return
+        class_name = line.split()[0]
+        if class_name not in self.__classes:
+            print("** class doesn't exist **")
+            return
+        inst = eval(class_name)()
+        inst.save()
+        print(inst.id)
 
     def emptyline(self):
         """Do nothing when an empty line is entered"""
