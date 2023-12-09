ea1572c5e7f23a37ca3daa5ea1427d1635dc979d
husamahmud <husamahmud@gmail.com>
2023-12-09 19:07:10 +0300

    [style] Improve efficiency of `pycodestyle` validation checks

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,8 @@ class BaseModel:
                 if key == '__class__':
                     continue
                 if key in ['created_at', 'updated_at']:
-                    setattr(self, key, datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key,
+                            datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, key, val)
         else:
