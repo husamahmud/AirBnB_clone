275a9d9a252f39be6764cbbd1b7d1dc2022af571
husamahmud <husamahmud@gmail.com>
2023-12-10 00:13:31 +0300

    [fix] Update `init` in `BaseModel`

--- models/base_model.py
+++ models/base_model.py
@@ -24,6 +24,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """Return a string representation of the BaseModel."""
